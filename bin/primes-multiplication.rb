66f2d33ce7cbfa7e9ec04669304c003a7b925c9b
Andrey Chernih <andrey.chernih@gmail.com>
2014-07-09 14:53:56 +0400

    Set a timeout value so that it don't stuck

--- bin/primes-multiplication.rb
+++ bin/primes-multiplication.rb
@@ -3,7 +3,9 @@ $LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
 require 'primes'
 require 'multiplication'
 require 'table_formatter'
+
 require 'benchmark'
+require 'timeout'
 
 $stderr.print "Give N: "
 n = STDIN.gets.strip.to_i
@@ -11,13 +13,20 @@ fail 'n <= 0' if n <= 0
 
 warn
 
-time = Benchmark.realtime {
-  primes = Primes.new(n).get
-  results = Multiplication.new(primes).results
-  formatter = TableFormatter.new(results, cols: primes, rows: primes)
+begin
+  Timeout.timeout(30) do
+    time = Benchmark.realtime {
+      primes = Primes.new(n).get
+      results = Multiplication.new(primes).results
+      formatter = TableFormatter.new(results, cols: primes, rows: primes)
 
-  puts formatter.format
-}
+      puts formatter.format
+    }
 
-warn
-warn "Finished in %.3fs" % time
+    warn
+    warn "Finished in %.3fs" % time
+  end
+rescue Timeout::Error
+  warn
+  warn 'We were unable to finish in a timely manner, try to give less value of N'
+end
